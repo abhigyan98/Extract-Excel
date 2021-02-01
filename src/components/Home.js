1c7449ec4dfd745554848f230726072c56a6d04b
abhigyan <abhigyanganguly73@gmailcom>
2021-02-01 23:32:40 +0530

    Adding Bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 //import { getData } from "../apicalls/apicalls";
-import {api} from "../Backend"
+import { api } from "../Backend"
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -26,34 +26,49 @@ const Home = () => {
       method: "POST",
       body: new FormData(form)
     })
-    .then((response) => {
-      return response.json();
-    })
-    .then((dt) => {
-      setData(dt)
-    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((dt) => {
+        setData(dt)
+      })
   }
 
   return (
-    <div>
-      <h1> Home </h1> <br /><br/>
-      <div>
-        <form id="myForm" encType="multipart/form-data" onSubmit={handleSubmit}>
-          <input type="file" name="file" id="file"/>
-          <button type="submit">Upload</button>
-        </form>
+
+    <div className="row">
+      <hr />
+      <h1 className="text-center text-success" > Extract Excel </h1>
+      <hr />
+      <br /><br />
+      <div className="col-sm-5">
+      <div className="card center border-0">
+        <div className="card-body">
+          <h5 className="card-title">Upload Excel File</h5>
+          <form id="myForm" encType="multipart/form-data" onSubmit={handleSubmit}>
+            <input type="file" name="file" id="file" /><br/><br/>
+            <button className="btn btn-primary" type="submit">Upload</button>
+          </form>
+        </div>
       </div>
-      {data.map((dt, index) => {
-          return (
+      </div>
+      <div className="col-sm-7">
+      <div className="card center border-0">
+        <span>
+          {data.map((dt, index) => {
+            return (
               <p key={index} align="left">[{
-                  dt.map((d, i) => {
-                      return (
-                      <span key={i}>{d}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                      )
-                  })
+                dt.map((d, i) => {
+                  return (
+                    <span key={i}>{d}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                  )
+                })
               }]</p>
-          )
-      })}
+            )
+          })}
+        </span>
+      </div>
+      </div>
     </div>
   );
 };
